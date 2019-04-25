3b4202eef28166729deff1be8b2013d995b1be9c
Damionmccoy <damion.c.mccoy@gmail.com>
2019-04-24 23:42:30 -0700

    Day 1 progress
    *Created 2 maps in tiled and added them to the main scene
    *Added a player character and created idle and walk animations
    *Created a camera script to move the camera while also bounding it to the room
    *Created triggers that change the cameras bounding box to the new room and moves the player to the new room
    *Created sign dialog and biome dialog and display them correctly

---
+++ Dungeon RPG/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Rigidbody2D playerRB;
+    private Animator playerAnim;
+    private Vector3 changePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changePos = Vector3.zero;
+        changePos.x = Input.GetAxisRaw("Horizontal");
+        changePos.y = Input.GetAxisRaw("Vertical");
+
+        if(changePos != Vector3.zero)
+        {
+            MovePlayer(changePos);
+        }
+        else
+        {
+            playerAnim.SetBool("moving", false);
+        }
+    }
+
+
+    /// <summary>
+    /// Moves the player around the screen using a vector3 and the movePosition function.
+    /// </summary>
+    /// <param name="_change">this is the vector3 used to decide how to move</param>
+    void MovePlayer(Vector3 _change)
+    {
+        Vector3 move = transform.position + _change * speed * Time.deltaTime;
+        
+        playerRB.MovePosition(move);
+        playerAnim.SetFloat("moveX", changePos.x);
+        playerAnim.SetFloat("moveY", changePos.y);
+        playerAnim.SetBool("moving", true);
+    }
+}
